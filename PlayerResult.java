e678dc5c07849e941795edcdc47624655eef8da8
0100910654kaz-beep <0100910654kaz@gmail.com>
2026-06-03 14:09:21 +0900

    Update PlayerResult.java

--- PlayerResult.java
+++ PlayerResult.java
@@ -3,39 +3,29 @@ package servlet;
 import java.io.Serializable;
 import java.util.Date;
 
-public class PlayerResult implements Serializable, Comparable<PlayerResult> {
+public class PlayerResult implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    private String プレイヤー名;
-    private Date 到達時刻;
-    private int ビンゴで描かれた数字;
+    private String playerName;
+    private Date reachTime;
+    private int drawnNumber;
 
     // コンストラクタ
-    public PlayerResult(String プレイヤー名, Date 到達時刻, int ビンゴで描かれた数字) {
-        this.プレイヤー名 = プレイヤー名;
-        this.到達時刻 = 到達時刻;
-        this.ビンゴで描かれた数字 = ビンゴで描かれた数字;
+    public PlayerResult(String playerName, Date reachTime, int drawnNumber) {
+        this.playerName = playerName;
+        this.reachTime = reachTime;
+        this.drawnNumber = drawnNumber;
     }
 
-    // 司会者画面やロジックで使うためのゲッターメソッド
     public String getPlayerName() {
-        return プレイヤー名;
+        return this.playerName;
     }
 
-    public Date 到達時刻() {
-        return 到達時刻;
+    public Date getReachTime() {
+        return this.reachTime;
     }
 
     public int getDrawnNumberAtBingo() {
-        return ビンゴで描かれた数字;
-    }
-
-    // 最新の達成者ほど上（リストの先頭）に並べるためのロジック
-    @Override
-    public int compareTo(PlayerResult 他の) {
-        if (他の.到達時刻() == null || this.到達時刻 == null) {
-            return 0;
-        }
-        return 他の.到達時刻().compareTo(this.到達時刻);
+        return this.drawnNumber;
     }
 }
