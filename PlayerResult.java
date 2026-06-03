301ae21e5bbf3d3b6044a82b1ca410bad265104d
0100910654kaz-beep <0100910654kaz@gmail.com>
2026-06-03 14:19:13 +0900

    Update PlayerResult.java

--- PlayerResult.java
+++ PlayerResult.java
@@ -3,29 +3,36 @@ package servlet;
 import java.io.Serializable;
 import java.util.Date;
 
-public class PlayerResult implements Serializable {
+public class PlayerResult implements Serializable, Comparable<PlayerResult> {
     private static final long serialVersionUID = 1L;
 
-    private String playerName;
-    private Date reachTime;
-    private int drawnNumber;
+    private String プレイヤー名;
+    private Date 到達時刻;
+    private int ビンゴで描かれた数字;
 
-    // コンストラクタ
-    public PlayerResult(String playerName, Date reachTime, int drawnNumber) {
-        this.playerName = playerName;
-        this.reachTime = reachTime;
-        this.drawnNumber = drawnNumber;
+    public PlayerResult(String プレイヤー名, Date 到達時刻, int ビンゴで描かれた数字) {
+        this.プレイヤー名 = プレイヤー名;
+        this.到達時刻 = 到達時刻;
+        this.ビンゴで描かれた数字 = ビンゴで描かれた数字;
     }
 
     public String getPlayerName() {
-        return this.playerName;
+        return プレイヤー名;
     }
 
-    public Date getReachTime() {
-        return this.reachTime;
+    public Date 到達時刻() {
+        return 到達時刻;
     }
 
     public int getDrawnNumberAtBingo() {
-        return this.drawnNumber;
+        return ビンゴで描かれた数字;
+    }
+
+    @Override
+    public int compareTo(PlayerResult 他の) {
+        if (他の.到達時刻() == null || this.到達時刻 == null) {
+            return 0;
+        }
+        return 他の.到達時刻().compareTo(this.到達時刻);
     }
 }
